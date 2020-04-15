fa76b3efd83f42d97ab5f4dc5e1ba20dd3d7d20f
Fernando Hernandez <hernandezfernando18982012@gmail.com>
2020-04-15 16:59:41 -0600

    commit prueba

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 # app.py
+# import flask
 from flask import Flask
-
+## creacion de la app
 app = Flask(__name__)
-
+# funcion saludo
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
-
+#funcion prueba de despliegue
 @app.route('/despliegue')
 def funcion2():
     return 'Funciono despliegue'
 
+#funcion de prueba final
+@app.route('/demo')
+def funcion3():
+    return 'Demostracion de despligue en aplicacion'
 
 if __name__ == '__main__':
+    #run server en el puerto 5000
     app.run(host='0.0.0.0')
